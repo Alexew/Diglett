5bd7b0b860971eded926d4be3c46be8caa708b0b
Alexew <gauba.alexew@gmail.com>
2025-05-13 15:50:51 +0300

    Add project files.

---
+++ Diglett.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Diglett.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Diglett.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
