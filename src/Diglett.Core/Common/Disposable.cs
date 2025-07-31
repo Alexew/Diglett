cc31c1983bc09d62b22f75d73b891c6eae220f81
Alexew <gauba.alexew@gmail.com>
2025-07-31 19:46:37 +0300

    Add abstract Disposable base class
    Introduces an abstract Disposable class implementing IDisposable with thread-safe disposal logic and an overridable OnDispose method for resource cleanup.

---
+++ src/Diglett.Core/Common/Disposable.cs
@@ -0,0 +1,20 @@
+﻿namespace Diglett
+{
+    public abstract class Disposable : IDisposable
+    {
+        private const int DisposedFlag = 1;
+        private int _isDisposed;
+
+        public void Dispose()
+        {
+            var wasDisposed = Interlocked.Exchange(ref _isDisposed, DisposedFlag);
+            if (wasDisposed == DisposedFlag)
+                return;
+
+            OnDispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void OnDispose(bool disposing) { }
+    }
+}
