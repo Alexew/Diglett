d63e9b5df072f4231653dd3c18ef025d09b251c4
Alexew <gauba.alexew@gmail.com>
2025-07-31 20:14:13 +0300

    Add IQueryableExtensions
    Introduces IQueryableExtensions with ApplyTracking and GetManyAsync methods to simplify entity tracking and batch retrieval by IDs. Enhances code reuse and consistency for querying entities in the data layer.

---
+++ src/Diglett.Core/Data/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Diglett.Core.Common;
+using Diglett.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diglett
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> ApplyTracking<T>(this IQueryable<T> query, bool tracked)
+            where T : BaseEntity
+        {
+            Guard.NotNull(query);
+
+            return tracked ? query.AsTracking() : query.AsNoTracking();
+        }
+
+        public static async Task<List<TEntity>> GetManyAsync<TEntity>(this IQueryable<TEntity> query, IEnumerable<int> ids, bool tracked = false)
+            where TEntity : BaseEntity
+        {
+            Guard.NotNull(query);
+            Guard.NotNull(ids);
+
+            if (!ids.Any())
+                return [];
+
+            var items = await query
+                .ApplyTracking(tracked)
+                .Where(a => ids.Contains(a.Id))
+                .ToListAsync();
+
+            return items.OrderBySequence(ids).ToList();
+        }
+    }
+}
