e24c91ba5cf8133efce3ce42558e3807ceb11d6b
Alexew <gauba.alexew@gmail.com>
2025-08-02 19:34:09 +0300

    Add abstract BaseTagHelper class
    Introduces a reusable abstract TagHelper base class with common logic for ID generation and output handling. Provides sealed overrides for synchronous and asynchronous processing, and extension points for derived tag helpers.

---
+++ src/Diglett.Core/Web/TagHelpers/BaseTagHelper.cs
@@ -0,0 +1,55 @@
+﻿#nullable disable
+
+using Diglett.Core.Utilities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Diglett.Web.TagHelpers
+{
+    public abstract class BaseTagHelper : TagHelper
+    {
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeNotBound]
+        public TagHelperOutput Output { get; set; }
+
+        public string Id { get; set; }
+
+        protected virtual string GenerateTagId(TagHelperContext context)
+            => context.TagName + '-' + CommonHelper.GenerateRandomDigitCode(10);
+
+        public override sealed void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            Output = output;
+            ProcessCommon(context, output);
+            ProcessCore(context, output);
+        }
+
+        public override sealed Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            Output = output;
+            ProcessCommon(context, output);
+            return ProcessCoreAsync(context, output);
+        }
+
+        private void ProcessCommon(TagHelperContext context, TagHelperOutput output)
+        {
+            Id = Id.NullEmpty() ?? GenerateTagId(context);
+
+            if (Id.HasValue())
+            {
+                output.Attributes.SetAttribute("id", Id);
+            }
+        }
+
+        protected virtual void ProcessCore(TagHelperContext context, TagHelperOutput output) { }
+        protected virtual Task ProcessCoreAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            ProcessCore(context, output);
+            return Task.CompletedTask;
+        }
+    }
+}
