2ed60ef5dc412d17a3ab2506c21cde3916de4d82
Alexew <gauba.alexew@gmail.com>
2025-08-02 17:35:22 +0300

    Add Dictionary extension methods
    Introduces TryRemove and AddRange extension methods for IDictionary types, providing convenient ways to remove items and add multiple entries with key uniqueness enforcement.

---
+++ src/Diglett.Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Diglett
+{
+    public static class DictionaryExtensions
+    {
+        public static bool TryRemove<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key, [MaybeNullWhen(false)] out TValue? value)
+            where TKey : notnull
+        {
+            value = default;
+
+            if (source is null || key is null)
+                return false;
+
+            if (source is ConcurrentDictionary<TKey, TValue> concurrentDict)
+                return concurrentDict.TryRemove(key, out value);
+
+            Guard.NotNull(source);
+
+            if (source.TryGetValue(key, out value))
+            {
+                source.Remove(key);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue?> values, IEnumerable<KeyValuePair<TKey, TValue?>> other)
+            where TKey : notnull
+        {
+            Guard.NotNull(values);
+            Guard.NotNull(other);
+
+            foreach (var kvp in other)
+            {
+                if (values.ContainsKey(kvp.Key))
+                {
+                    throw new ArgumentException("An item with the same key has already been added.");
+                }
+
+                values.Add(kvp);
+            }
+        }
+    }
+}
