7de7088c71df0f10544008fa077480a00a328285
Alexew <gauba.alexew@gmail.com>
2025-08-02 19:32:12 +0300

    Add WebHelper and IHtmlContentExtensions utilities
    Introduces WebHelper for HTML encoding and IHtmlContentExtensions for converting IHtmlContent to HtmlString and checking for content presence. These utilities enhance HTML content handling and encoding in the web platform.

---
+++ src/Diglett.Core/Http/WebHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Diglett.Core.Http
+{
+    public static class WebHelper
+    {
+        private static HtmlEncoder? _htmlEncoder;
+
+        public static HtmlEncoder HtmlEncoder
+        {
+            get
+            {
+                if (_htmlEncoder != null)
+                    return _htmlEncoder;
+
+                return HtmlEncoder.Create(new TextEncoderSettings(UnicodeRanges.All));
+            }
+            set => _htmlEncoder = value;
+        }
+    }
+}
---
+++ src/Diglett.Core/Platform/Web/IHtmlContentExtensions.cs
@@ -0,0 +1,92 @@
+﻿using Diglett.Core.Http;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+using System.Text;
+
+namespace Diglett.Core.Web
+{
+    public static class IHtmlContentExtensions
+    {
+        public static HtmlString ToHtmlString(this IHtmlContent content)
+        {
+            Guard.NotNull(content);
+
+            if (content is HtmlString htmlString)
+            {
+                return htmlString;
+            }
+            else
+            {
+                var sb = new StringBuilder(100);
+                using var writer = new StringWriter(sb);
+                content.WriteTo(writer, WebHelper.HtmlEncoder);
+
+                return new HtmlString(writer.ToString());
+            }
+        }
+
+        public static bool HasContent(this IHtmlContent content)
+        {
+            Guard.NotNull(content);
+
+            switch (content)
+            {
+                case TagBuilder:
+                case LocalizedHtmlString:
+                case TagHelperAttribute:
+                    return true;
+                case HtmlString htmlString:
+                    return htmlString.Value.HasValue();
+                case LocalizedString locString:
+                    return locString.Value.HasValue();
+                case TagHelperContent tagHelperContent:
+                    return !tagHelperContent.IsEmptyOrWhiteSpace;
+                case HtmlContentBuilder builder:
+                    if (builder.Count == 0)
+                        return false;
+                    break;
+                case TagHelperOutput output:
+                    var hasValue = output.TagName.HasValue()
+                        || !output.Content.IsEmptyOrWhiteSpace
+                        || !output.PreElement.IsEmptyOrWhiteSpace
+                        || !output.PreContent.IsEmptyOrWhiteSpace
+                        || !output.PostContent.IsEmptyOrWhiteSpace
+                        || !output.PostElement.IsEmptyOrWhiteSpace;
+
+                    return hasValue;
+            }
+
+            using var writer = new HasContentWriter();
+
+            content.WriteTo(writer, NullHtmlEncoder.Default);
+
+            return writer.HasContent;
+        }
+
+        class HasContentWriter : TextWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+
+            public bool HasContent { get; private set; }
+
+            public override void Write(char value)
+            {
+                if (!HasContent && !char.IsWhiteSpace(value))
+                {
+                    HasContent = true;
+                }
+            }
+
+            public override void Write(string? value)
+            {
+                if (!HasContent && !string.IsNullOrWhiteSpace(value))
+                {
+                    HasContent = true;
+                }
+            }
+        }
+    }
+}
