1cd951729e351303eace67040da7d73d5d19c206
Alexew <gauba.alexew@gmail.com>
2025-08-02 14:40:35 +0300

    Add IfTagHelper for conditional rendering in views
    Introduces a custom TagHelper (IfTagHelper) to enable conditional rendering in Razor views using the 'dt-if' attribute. Updates project references and _ViewImports.cshtml to register the new TagHelper.

---
+++ src/Diglett.Core/Web/TagHelpers/IfTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Diglett.Web.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = IfAttributeName)]
+    public class IfTagHelper : TagHelper
+    {
+        const string IfAttributeName = "dt-if";
+
+        public override int Order => int.MinValue;
+
+        [HtmlAttributeName(IfAttributeName)]
+        public bool Condition { get; set; } = true;
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Condition)
+            {
+                output.SuppressOutput();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
