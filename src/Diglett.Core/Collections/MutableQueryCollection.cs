6a28d607fe83feb9389861e4048588a414fc6e5f
Alexew <gauba.alexew@gmail.com>
2025-08-02 17:40:11 +0300

    Add MutableQueryCollection
    Introduces MutableQueryCollection for mutable query string manipulation and QueryStringExtensions for merging query strings. These additions enhance query string handling and provide utility methods for combining and modifying query parameters.

---
+++ src/Diglett.Core/Collections/MutableQueryCollection.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
+namespace Diglett.Core.Collections
+{
+    public class MutableQueryCollection : QueryCollection
+    {
+        private readonly Dictionary<string, StringValues> _store;
+
+        public MutableQueryCollection()
+            : this(new Dictionary<string, StringValues>(StringComparer.OrdinalIgnoreCase)) { }
+
+        public MutableQueryCollection(QueryString queryString)
+            : this(queryString.ToString()) { }
+
+        public MutableQueryCollection(string queryString)
+            : this(QueryHelpers.ParseQuery(queryString)) { }
+
+        public MutableQueryCollection(Dictionary<string, StringValues> store)
+            : base(store)
+        {
+            Guard.NotNull(store);
+
+            _store = store;
+        }
+
+        public Dictionary<string, StringValues> Store
+        {
+            get => _store;
+        }
+
+        public virtual MutableQueryCollection Add(string name, string value, bool isUnique = false)
+        {
+            Guard.NotEmpty(name);
+
+            if (_store.TryGetValue(name, out var existingValues))
+            {
+                var passedValues = new StringValues(value.SplitSafe(',').ToArray());
+                _store[name] = isUnique ? passedValues : StringValues.Concat(existingValues, passedValues);
+            }
+            else
+            {
+                _store.Add(name, value);
+            }
+
+            return this;
+        }
+
+        public virtual MutableQueryCollection Add(string name, StringValues values, bool isUnique = false)
+        {
+            Guard.NotEmpty(name);
+
+            if (_store.TryGetValue(name, out var existingValues))
+            {
+                _store[name] = isUnique ? values : StringValues.Concat(existingValues, values);
+            }
+            else
+            {
+                _store.Add(name, values);
+            }
+
+            return this;
+        }
+
+        public MutableQueryCollection Remove(string name)
+        {
+            Guard.NotEmpty(name);
+            _store.TryRemove(name, out _);
+            return this;
+        }
+
+        public MutableQueryCollection Clear()
+        {
+            _store.Clear();
+            return this;
+        }
+
+        public QueryString ToQueryString()
+        {
+            return QueryString.Create(_store);
+        }
+
+        public override string ToString()
+        {
+            return ToQueryString().Value ?? string.Empty;
+        }
+    }
+}
---
+++ src/Diglett.Core/Http/QueryStringExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Diglett.Core.Collections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
+namespace Diglett
+{
+    public static class QueryStringExtensions
+    {
+        public static QueryString Merge(this QueryString source, string? other)
+        {
+            if (!string.IsNullOrEmpty(other) && other[0] != '?')
+                return Merge(source, new QueryString('?' + other));
+
+            return Merge(source, new QueryString(other));
+        }
+
+        public static QueryString Merge(this QueryString source, QueryString other)
+        {
+            if (!source.HasValue)
+                return other;
+
+            if (!other.HasValue)
+                return source;
+
+            return MergeQueryStringInternal(
+                new MutableQueryCollection(source.Value),
+                other);
+        }
+
+        public static QueryString Merge(this IQueryCollection? source, string? other)
+        {
+            if (!string.IsNullOrEmpty(other) && other[0] != '?')
+                return Merge(source, new QueryString('?' + other));
+
+            return Merge(source, new QueryString(other));
+        }
+
+        public static QueryString Merge(this IQueryCollection? source, QueryString other)
+        {
+            if (source == null || source.Count == 0)
+                return other;
+
+            if (!other.HasValue)
+                return QueryString.Empty;
+
+            return MergeQueryStringInternal(
+                new MutableQueryCollection(new Dictionary<string, StringValues>(source)),
+                other);
+        }
+
+        private static QueryString MergeQueryStringInternal(MutableQueryCollection source, QueryString other)
+        {
+            var modify = QueryHelpers.ParseQuery(other.Value);
+
+            foreach (var kvp in modify)
+            {
+                source.Add(kvp.Key, kvp.Value, true);
+            }
+
+            return source.ToQueryString();
+        }
+    }
+}
