94608ae306e2ed0d79ec0904cc5273eb1a5e3c15
Alexew <gauba.alexew@gmail.com>
2025-08-02 17:52:07 +0300

    Add DiglettRazorPage base class for Razor views
    Introduces DiglettRazorPage<TModel> to provide convenient access to HttpRequest, IWebHelper, and service resolution in Razor views. Updates _ViewImports.cshtml to inherit from DiglettRazorPage and include necessary namespaces.

---
+++ src/Diglett.Core/Web/Razor/DiglettRazorPage.cs
@@ -0,0 +1,24 @@
+﻿using Diglett.Core.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Diglett.Web.Razor
+{
+    public abstract class DiglettRazorPage<TModel> : RazorPage<TModel>
+    {
+        private IWebHelper? _webHelper;
+
+        protected HttpRequest Request
+        {
+            get => Context.Request;
+        }
+
+        protected IWebHelper WebHelper => _webHelper ??= Context.RequestServices.GetRequiredService<IWebHelper>();
+
+        protected T? Resolve<T>() where T : notnull
+        {
+            return Context.RequestServices.GetService<T>();
+        }
+    }
+}
