e3abde375f8fc5ce0c16623946962caf92265ffe
Alexew <gauba.alexew@gmail.com>
2025-08-03 14:51:35 +0300

    Add custom model binder for CatalogSearchQuery
    Introduces CatalogSearchQueryModelBinder to enable custom binding for CatalogSearchQuery. Updates CatalogSearchQuery to use the new model binder and disables validation with ValidateNever.

--- src/Diglett.Core/Catalog/Search/CatalogSearchQuery.cs
+++ src/Diglett.Core/Catalog/Search/CatalogSearchQuery.cs
@@ -1,7 +1,11 @@
-﻿using Diglett.Core.Search;
+﻿using Diglett.Core.Catalog.Search.Modelling;
+using Diglett.Core.Search;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Diglett.Core.Catalog.Search
 {
+    [ValidateNever, ModelBinder(typeof(CatalogSearchQueryModelBinder))]
     public class CatalogSearchQuery : SearchQuery<CatalogSearchQuery>
     {
     }
---
+++ src/Diglett.Core/Catalog/Search/Modelling/CatalogSearchQueryModelBinder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Diglett.Core.Catalog.Search.Modelling
+{
+    public class CatalogSearchQueryModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            Guard.NotNull(bindingContext);
+
+            var factory = bindingContext.HttpContext.RequestServices.GetService<ICatalogSearchQueryFactory>();
+
+            if (factory == null)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+            }
+            else if (factory.Current != null)
+            {
+                bindingContext.Result = ModelBindingResult.Success(factory.Current);
+            }
+            else if (bindingContext.ModelType != typeof(CatalogSearchQuery))
+            {
+                bindingContext.Result = ModelBindingResult.Success(new CatalogSearchQuery());
+            }
+            else
+            {
+                var query = factory.CreateFromQuery();
+
+                if (query == null)
+                {
+                    bindingContext.Result = ModelBindingResult.Failed();
+                }
+                else
+                {
+                    bindingContext.Result = ModelBindingResult.Success(query);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
