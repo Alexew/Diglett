965035bca492ab86661b2e84071a5bf1972c2408
Alexew <gauba.alexew@gmail.com>
2025-08-03 14:35:34 +0300

    Add centralized error helper class
    Introduces a static Error class providing factory methods for common exception types, including ApplicationException, ArgumentOutOfRangeException, ArgumentException, InvalidCastException, and InvalidOperationException. This centralizes error creation and message formatting for improved consistency.

---
+++ src/Diglett.Core/Common/Error.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace Diglett
+{
+    public static class Error
+    {
+        public static Exception Application(string message, params object[] args)
+        {
+            return new ApplicationException(message.FormatCurrent(args));
+        }
+
+        public static Exception Application(Exception innerException, string message, params object[] args)
+        {
+            return new ApplicationException(message.FormatCurrent(args), innerException);
+        }
+
+        public static Exception ArgumentOutOfRange<T>(Func<T> arg)
+        {
+            return new ArgumentOutOfRangeException(arg.Method.Name);
+        }
+
+        public static Exception ArgumentOutOfRange(string argName)
+        {
+            return new ArgumentOutOfRangeException(argName);
+        }
+
+        public static Exception ArgumentOutOfRange(string argName, string message, params object[] args)
+        {
+            return new ArgumentOutOfRangeException(argName, string.Format(CultureInfo.CurrentCulture, message, args));
+        }
+
+        public static Exception Argument(string argName, string message, params object[] args)
+        {
+            return new ArgumentException(string.Format(CultureInfo.CurrentCulture, message, args), argName);
+        }
+
+        public static Exception InvalidCast(Type fromType, Type toType)
+        {
+            return InvalidCast(fromType, toType, null);
+        }
+
+        public static Exception InvalidCast(Type fromType, Type toType, Exception? innerException)
+        {
+            return new InvalidCastException("Cannot convert from type '{0}' to '{1}'.".FormatCurrent(fromType?.FullName ?? "NULL", toType.FullName), innerException);
+        }
+
+        public static Exception NoElements()
+        {
+            return new InvalidOperationException("Sequence contains no elements.");
+        }
+
+        public static Exception MoreThanOneElement()
+        {
+            return new InvalidOperationException("Sequence contains more than one element.");
+        }
+    }
+}
